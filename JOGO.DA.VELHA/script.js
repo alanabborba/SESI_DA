7978513d42bc239e7545b9d05fd4b4406a9fd465
Alana Bosa Borba <alana.b.borba@aluno.senai.br>
2026-09-01 09:10:09 -0300

    jogo.velha

---
+++ JOGO.DA.VELHA/script.js
@@ -0,0 +1,36 @@
+const casas = document.querySelectorAll(".casa");
+const mensagem = document.querySelector("#mensagem");
+
+let jogador = "X";
+
+const vitorias = [
+    [0,1,2], [3,4,5], [6,7,8],
+    [0,3,6], [1,4,7], [2,5,8],
+    [0,4,8], [2,4,6]
+];
+
+casas.forEach(casa => {
+    casa.onclick = () => {
+        if (casa.textContent) return;
+
+        casa.textContent = jogador;
+
+        for (let [a,b,c] of vitorias) {
+            if (
+                casas[a].textContent &&
+                casas[a].textContent == casas[b].textContent &&
+                casas[a].textContent == casas[c].textContent
+            ) {
+                alert("Jogador " + jogador + " venceu!");
+
+                casas.forEach(c => c.textContent = "");
+                jogador = "X";
+                mensagem.textContent = "Vez do jogador X";
+                return;
+            }
+        }
+
+        jogador = jogador == "X" ? "O" : "X";
+        mensagem.textContent = "Vez do jogador " + jogador;
+    };
+});
